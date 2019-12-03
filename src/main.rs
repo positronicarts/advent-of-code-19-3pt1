1300ea0c0ce15ab81d824281cb2aef2439c42dc2
Murray Rogers <murray.rogers@metaswitch.com>
2019-12-03 17:50:05 +0000

    Tidy code

--- src/main.rs
+++ src/main.rs
@@ -11,119 +11,69 @@ impl std::str::FromStr for Instruction {
 
     fn from_str(s: &str) -> Result<Self, ()> {
         let mut chars = s.chars();
-        let direction : char = chars.next().unwrap();
-        let num : i32 = chars.collect::<String>().parse().unwrap();
+        let direction: char = chars.next().unwrap();
+        let num: i32 = chars.collect::<String>().parse().unwrap();
 
         match direction {
             'U' => Ok(Instruction::Up(num)),
             'D' => Ok(Instruction::Down(num)),
             'L' => Ok(Instruction::Left(num)),
             'R' => Ok(Instruction::Right(num)),
-            _ => Err(())
+            _ => Err(()),
         }
     }
+}
+
+fn handle_instructions(
+    grid: &mut std::collections::HashMap<(i64, i64), u16>,
+    instructions: Vec<Instruction>,
+    bitval: u16,
+) {
+    let (mut x, mut y): (i64, i64) = (0, 0);
+
+    for instruction in &instructions {
+        let (dist, movement): (&i32, Box<dyn Fn(i64, i64) -> (i64, i64)>) = match instruction {
+            Instruction::Up(dist) => (dist, Box::new(|x, y| (x, y + 1))),
+            Instruction::Down(dist) => (dist, Box::new(|x, y| (x, y - 1))),
+            Instruction::Left(dist) => (dist, Box::new(|x, y| (x - 1, y))),
+            Instruction::Right(dist) => (dist, Box::new(|x, y| (x + 1, y))),
+        };
+
+        for _ii in 0..*dist {
+            let new_coords = movement(x, y);
+            x = new_coords.0;
+            y = new_coords.1;
+            let val = grid.get(&(x, y)).map_or_else(|| bitval, |val| val | bitval);
+            grid.insert((x, y), val);
+        }
+    }
+}
+
+fn extract_instructions<'a>(lines: &mut std::str::Lines<'a>) -> Vec<Instruction> {
+    lines
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|x| x.to_string().parse().unwrap())
+        .collect()
 }
 
 fn main() {
     let content = std::fs::read_to_string("inputs.txt").unwrap();
     let mut lines = content.lines();
+    let (i1, i2) = (extract_instructions(&mut lines), extract_instructions(&mut lines));
+    
+    let mut grid: std::collections::HashMap<(i64, i64), u16> = std::collections::HashMap::new();
+    handle_instructions(&mut grid, i1, 1);
+    handle_instructions(&mut grid, i2, 2);
 
-    let wire1 : Vec<String> = lines.next().unwrap().split(",").map(|x| x.to_string()).collect();
-    let wire2 : Vec<String> = lines.next().unwrap().split(",").map(|x| x.to_string()).collect();
-
-    let instructions1 : Vec<Instruction> = wire1.into_iter().map(|s| s.parse().unwrap()).collect();
-    let instructions2 : Vec<Instruction> = wire2.into_iter().map(|s| s.parse().unwrap()).collect();
-
-    let mut grid : std::collections::HashMap<(i64, i64), u16> = std::collections::HashMap::new();
-
-    let mut x : i64 = 0;
-    let mut y : i64 = 0;
-
-    for instruction in &instructions1 {
-        match instruction {
-            Instruction::Up(distance) => {
-                for _ii in 0..*distance {
-                    y += 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 1, |val| val | 1);
-                    grid.insert((x, y), val);
-                }
-            }
-            Instruction::Down(distance) => {
-                for _ii in 0..*distance {
-                    y -= 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 1, |val| val | 1);
-                    grid.insert((x, y), val);
-                }
-            }     
-            Instruction::Left(distance) => {
-                for _ii in 0..*distance {
-                    x -= 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 1, |val| val | 1);
-                    grid.insert((x, y), val);
-                }
-            }           
-            Instruction::Right(distance) => {
-                for _ii in 0..*distance {
-                    x += 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 1, |val| val | 1);
-                    grid.insert((x, y), val);
-                }
-            }       
-        }
-    }
-
-    let mut x : i64 = 0;
-    let mut y : i64 = 0;
-
-    for instruction in &instructions2 {
-        match instruction {
-            Instruction::Up(distance) => {
-                for _ii in 0..*distance {
-                    y += 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 2, |val| val | 2);
-                    grid.insert((x, y), val);
-                }
-            }
-            Instruction::Down(distance) => {
-                for _ii in 0..*distance {
-                    y -= 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 2, |val| val | 2);
-                    grid.insert((x, y), val);
-                }
-            }     
-            Instruction::Left(distance) => {
-                for _ii in 0..*distance {
-                    x -= 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 2, |val| val | 2);
-                    grid.insert((x, y), val);
-                }
-            }           
-            Instruction::Right(distance) => {
-                for _ii in 0..*distance {
-                    x += 1;
-                    let val = grid.get(&(x, y)).map_or_else(|| 2, |val| val | 2);
-                    grid.insert((x, y), val);
-                }
-            }       
-        }
-    }    
-
-    let mut dist = None;
-
-    for ((x, y), val) in grid {
-        if ((val & 1) == 1) && ((val & 2) == 2) && ((x != 0) || (y != 0)) {
-            match dist {
-                None => {
-                    dist = Some(x.abs() + y.abs());
-                }
-                Some(d) => { 
-                    if (x.abs() + y.abs()) < d {
-                        dist = Some(x.abs() + y.abs());
-                    }
-                }
-            }
-        }
-    }
-
-    println!("{}", dist.unwrap());
+    println!(
+        "{}",
+        grid.iter()
+            .filter(|(_, val)| **val == 3)
+            .filter(|((x, y), _)| *x != 0 || *y != 0)
+            .map(|((x, y), _)| x.abs() + y.abs())
+            .min()
+            .unwrap()
+    );
 }
